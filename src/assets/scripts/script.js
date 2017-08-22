72c24cb505c5128c9c9ed14555ef821dac0fe1f6
radames-rex <radames.rex@gmail.com>
2017-08-22 17:23:38 -0300

    smooth scrolling

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,3 +1,4 @@
+// Reduz Header após scroll
 $( window ).scroll(function() {
 	var top = $('.header').offset().top <= 120;
   if(top){
@@ -6,3 +7,24 @@ $( window ).scroll(function() {
   	$('.header-image').addClass('header-image-mini'); 	
   }
 });
+
+// Smooth Scroll com a navegação dos links
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    }
+  });
